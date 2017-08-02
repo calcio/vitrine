bc2db8d1048d7993a1f2ed5e515d7df86e9c4cb5
Cálcio <calcionit@gmail.com>
2017-08-02 18:39:09 -0300

    removendo ip do vangrant

--- config/web.php
+++ config/web.php
@@ -69,13 +69,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '172.20.50.*'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '172.20.50.*'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
