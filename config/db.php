f1f372430438fdbe682588587f0f2264332cd2e5
Cálcio <calcionit@gmail.com>
2017-08-26 16:35:29 -0300

    Correção do formatter do config.php e criação dos Models e CRUD pelo Gii

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=vitrine',
-    'username' => 'root',
-    'password' => 'BdLnx#1404',
+    'username' => 'meetup',
+    'password' => 'meetup',
     'charset' => 'utf8',
 ];
